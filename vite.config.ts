43da0a3e40c1eba6d2a0ff1c63cb5a17f5726bd0
Ronan Morais <ronan86@gmail.com>
2023-05-22 16:19:31 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({adapter: vercel()})],
 });
+function vercel(): string | import("solid-start/vite").Adapter | undefined {
+  throw new Error("Function not implemented.");
+}
+
