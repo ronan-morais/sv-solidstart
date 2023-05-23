40ba69eda90e0b1d96df097941343ddddb09fb77
Ronan Morais <ronan86@gmail.com>
2023-05-23 15:42:33 -0300

    update node

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,5 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({adapter: vercel()})],
+  plugins: [solid()],
 });
-
-function vercel(): string | import("solid-start/vite").Adapter | undefined {
-  throw new Error("Function not implemented.");
-}
-
-
